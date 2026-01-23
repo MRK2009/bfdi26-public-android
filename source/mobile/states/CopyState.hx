88f6298acc643fe77e3978d3dd3d3633955c78ca
TheLagKing <dalucanogheaven12@gmail.com>
2026-01-23 19:33:58 +0800

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -1,7 +1,7 @@
 package mobile.states;
 
 #if mobile
-import TitleState;
+import funkin.states.Title;
 import lime.utils.Assets as LimeAssets;
 import openfl.utils.Assets as OpenFLAssets;
 import openfl.utils.ByteArray;
