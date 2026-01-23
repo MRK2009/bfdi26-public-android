9152d79a0293c020c0601197d56c9a0e731e30e1
TheLagKing <dalucanogheaven12@gmail.com>
2026-01-23 19:00:59 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -58,6 +58,21 @@ import sys.thread.Thread;
 import crowplexus.iris.Iris;
 #end
 
+import mobile.backend.SUtil;
+ 
+ //Android
+ #if android
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
+ #end
+
 //Flixel
 import flixel.sound.FlxSound;
 import flixel.FlxG;
