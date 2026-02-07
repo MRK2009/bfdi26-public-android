2477dd7cdd07c64542bb28f34d3be0a0b24dc522
TheLagKing <dalucanogheaven12@gmail.com>
2026-02-07 09:49:19 +0800

    Update BaseOptionsMenu.hx

--- source/funkin/states/options/BaseOptionsMenu.hx
+++ source/funkin/states/options/BaseOptionsMenu.hx
@@ -6,7 +6,6 @@ import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.gamepad.FlxGamepadManager;
 
 import funkin.backend.MusicBeatState;
-import funkin.backend.MusicBeatSubstate;
 import funkin.objects.CheckboxThingie;
 import funkin.objects.AttachedText;
 import funkin.states.options.Option;
