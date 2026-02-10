2be2e9e360ff20c54600aec166446a1aff3f206f
TheLagKing <dalucanogheaven12@gmail.com>
2026-02-10 12:16:20 +0800

    Update ControlsSubState.hx

--- source/funkin/states/options/ControlsSubState.hx
+++ source/funkin/states/options/ControlsSubState.hx
@@ -177,6 +177,11 @@ class ControlsSubState extends MusicBeatSubstate
 		border.cameras = [funkin.states.Title.quickCreateCam()];
 		border.scrollFactor.set();
 		add(border);
+
+		#if mobile
+		addVirtualPad(LEFT_FULL, A_B);
+		addVirtualPadCamera();
+		#end
 	}
 
 	var lastID:Int = 0;
@@ -352,12 +357,12 @@ class ControlsSubState extends MusicBeatSubstate
 			// if(FlxG.keys.justPressed.CONTROL || FlxG.gamepads.anyJustPressed(LEFT_SHOULDER) || FlxG.gamepads.anyJustPressed(RIGHT_SHOULDER)) swapMode();
 
 			if(FlxG.keys.justPressed.LEFT || FlxG.keys.justPressed.RIGHT || FlxG.gamepads.anyJustPressed(DPAD_LEFT) || FlxG.gamepads.anyJustPressed(DPAD_RIGHT) ||
-				FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_LEFT) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_RIGHT)) updateAlt(true);
+				FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_LEFT) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_RIGHT) #if mobile || virtualPad.buttonLeft.justPressed || virtualPad.buttonRight.justPressed #end) updateAlt(true);
 
-			if(FlxG.keys.justPressed.UP || FlxG.gamepads.anyJustPressed(DPAD_UP) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_UP)) updateText(-1);
-			else if(FlxG.keys.justPressed.DOWN || FlxG.gamepads.anyJustPressed(DPAD_DOWN) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_DOWN)) updateText(1);
+			if(FlxG.keys.justPressed.UP || FlxG.gamepads.anyJustPressed(DPAD_UP) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_UP) #if mobile || virtualPad.buttonUp.justPressed #end) updateText(-1);
+			else if(FlxG.keys.justPressed.DOWN || FlxG.gamepads.anyJustPressed(DPAD_DOWN) || FlxG.gamepads.anyJustPressed(LEFT_STICK_DIGITAL_DOWN) #if mobile || virtualPad.buttonDown.justPressed #end) updateText(1);
 
-			if(FlxG.keys.justPressed.ENTER || FlxG.gamepads.anyJustPressed(START) || FlxG.gamepads.anyJustPressed(A))
+			if(FlxG.keys.justPressed.ENTER || FlxG.gamepads.anyJustPressed(START) || FlxG.gamepads.anyJustPressed(A) #if mobile || virtualPad.buttonA.justPressed #end)
 			{
 				if(options[curOptions[curSelected]][1] != defaultKey)
 				{
@@ -404,7 +409,7 @@ class ControlsSubState extends MusicBeatSubstate
 		{
 			var altNum:Int = curAlt ? 1 : 0;
 			var curOption:Array<Dynamic> = options[curOptions[curSelected]];
-			if(FlxG.keys.pressed.ESCAPE || FlxG.gamepads.anyPressed(B))
+			if(FlxG.keys.pressed.ESCAPE || FlxG.gamepads.anyPressed(B) #if mobile || virtualPad.buttonB.pressed #end)
 			{
 				holdingEsc += elapsed;
 				if(holdingEsc > 0.5)
@@ -413,7 +418,7 @@ class ControlsSubState extends MusicBeatSubstate
 					closeBinding();
 				}
 			}
-			else if (FlxG.keys.pressed.BACKSPACE || FlxG.gamepads.anyPressed(BACK))
+			else if (FlxG.keys.pressed.BACKSPACE || FlxG.gamepads.anyPressed(BACK) #if mobile || virtualPad.buttonB.pressed #end)
 			{
 				holdingEsc += elapsed;
 				if(holdingEsc > 0.5)
