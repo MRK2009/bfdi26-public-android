1a8ddb7aa2e78b4d2bd6c6b196d2334d0173ae47
TheLagKing <dalucanogheaven12@gmail.com>
2026-02-13 08:50:38 +0800

    Update BaseStage.hx

--- source/funkin/backend/BaseStage.hx
+++ source/funkin/backend/BaseStage.hx
@@ -88,7 +88,6 @@ class BaseStage extends FlxBasic
 	public function eventPushedUnique(event:EventNote) {}
 
 	// Note Hit/Miss
-	public function goodNoteHit(note:Note) {}
 	public function opponentNoteHit(note:Note) {}
 	public function noteMiss(note:Note) {}
 	public function noteMissPress(direction:Int) {}
