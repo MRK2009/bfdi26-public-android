240240f7917c1aaa73d76c3b066997ff7c80da5a
TheLagKing <dalucanogheaven12@gmail.com>
2026-02-19 08:07:21 +0800

    Update OptionsState.hx

--- source/funkin/states/options/OptionsState.hx
+++ source/funkin/states/options/OptionsState.hx
@@ -116,10 +116,7 @@ class OptionsState extends MusicBeatState
 			}
 			else FlxG.switchState(funkin.states.NewMain.new);
 		}
-		else if (controls.ACCEPT #if mobile || virtualPad.buttonA.justPressed #end)
-		{
-		openSelectedSubstate(options[curSelected]);
-		}
+		else if (controls.ACCEPT #if mobile || virtualPad.buttonA.justReleased #end) openSelectedSubstate(options[curSelected]);
 	}
 	
 	function changeSelection(change:Int = 0) 
